128a0689bd1637321ef4acdd738a8e55697f3a2f
孙汇洲 <hzhou@citiz.net>
2017-10-26 13:23:58 -0700

    2017年10月26日整个tensorflow ssd都可以编译支持armv7,armv7s和i386

--- tensorflow/contrib/makefile/compile_ios_tensorflow_s.sh
+++ tensorflow/contrib/makefile/compile_ios_tensorflow_s.sh
@@ -48,6 +48,22 @@ LIBDIR=${GENDIR}lib
 LIB_PREFIX=libtensorflow-core
 
 make -j"${JOB_COUNT}" -f tensorflow/contrib/makefile/Makefile_ios \
+TARGET=IOS IOS_ARCH=ARMV7 LIB_NAME=${LIB_PREFIX}-armv7.a OPTFLAGS="$1"
+if [ $? -ne 0 ]
+then
+echo "armv7 compilation failed."
+exit 1
+fi
+
+make -j"${JOB_COUNT}" -f tensorflow/contrib/makefile/Makefile_ios \
+TARGET=IOS IOS_ARCH=ARMV7S LIB_NAME=${LIB_PREFIX}-armv7s.a OPTFLAGS="$1"
+if [ $? -ne 0 ]
+then
+echo "armv7s compilation failed."
+exit 1
+fi
+
+make -j"${JOB_COUNT}" -f tensorflow/contrib/makefile/Makefile_ios \
 TARGET=IOS IOS_ARCH=ARM64 LIB_NAME=${LIB_PREFIX}-arm64.a OPTFLAGS="$1"
 if [ $? -ne 0 ]
 then
@@ -63,8 +79,19 @@ then
   exit 1
 fi
 
+make -j"${JOB_COUNT}" -f tensorflow/contrib/makefile/Makefile_ios \
+TARGET=IOS IOS_ARCH=I386 LIB_NAME=${LIB_PREFIX}-i386.a OPTFLAGS="$1"
+if [ $? -ne 0 ]
+then
+echo "i386 compilation failed."
+exit 1
+fi
+
 lipo \
 ${LIBDIR}/ios_ARM64/${LIB_PREFIX}-arm64.a \
+${LIBDIR}/ios_ARMV7/${LIB_PREFIX}-armv7.a \
+${LIBDIR}/ios_ARMV7S/${LIB_PREFIX}-armv7s.a \
 ${LIBDIR}/ios_X86_64/${LIB_PREFIX}-x86_64.a \
+${LIBDIR}/ios_I386/${LIB_PREFIX}-i386.a \
 -create \
 -output ${LIBDIR}/${LIB_PREFIX}.a
